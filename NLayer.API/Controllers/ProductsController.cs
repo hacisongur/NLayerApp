67c6d495807d1fa5029c66c8f95bb7c4b44ee4e7
hacisongur <hacisongur@inspiria.vip>
2023-03-18 13:33:09 +0300

    AutoFac

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -22,25 +22,38 @@ namespace NLayer.API.Controllers
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory()
         {
+
             return CreateActionResult(await _service.GetProductsWithCategory());
         }
+
+
+
+
 
         /// GET api/products
         [HttpGet]
         public async Task<IActionResult> All()
         {
-            var products=await _service.GetAllAsync();
-            var productsDtos=_mapper.Map<List<ProductDto>>(products.ToList());
-            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200,productsDtos));
+            var products = await _service.GetAllAsync();
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
         }
+
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))]
         // GET /api/products/5
         [HttpGet("{id}")]
-        public async Task <IActionResult> GetById(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            var product=await _service.GetByIdAsync(id);
+
+
+            var product = await _service.GetByIdAsync(id);
             var productsDto = _mapper.Map<ProductDto>(product);
-            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDto)); 
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDto));
         }
+
+
+
         [HttpPost]
         public async Task<IActionResult> Save(ProductDto productDto)
         {
@@ -48,21 +61,28 @@ namespace NLayer.API.Controllers
             var productsDto = _mapper.Map<ProductDto>(product);
             return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productsDto));
         }
+
+
         [HttpPut]
         public async Task<IActionResult> Update(ProductUpdateDto productDto)
-        {//Geriye birşey dönmüyor //Data almadığında nocontent
+        {
             await _service.UpdateAsync(_mapper.Map<Product>(productDto));
 
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
         }
+
         // DELETE api/products/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Remove(int id)
         {
             var product = await _service.GetByIdAsync(id);
+
+
+
+
             await _service.RemoveAsync(product);
+
             return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
-            //Boş sınıf dönüyoruz
         }
     }
 }
